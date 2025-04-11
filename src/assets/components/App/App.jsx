c8225e075ab41e41f28fb9e9254c71b061c0f839
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-11 10:32:17 +0330

    update NavBar.jsx and App.jsx

--- src/assets/components/App/App.jsx
+++ src/assets/components/App/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import './App.css'
+import NavBar from '../NavBar/NavBar'
 export default function App() {
   return (
-    <div>Hello</div>
+    <>
+      <NavBar/>
+    </>
   )
 }
 
