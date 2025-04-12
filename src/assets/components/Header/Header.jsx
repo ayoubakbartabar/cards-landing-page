5b7875df9730583f0d9753708f641e5e499f6a88
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-12 20:09:35 +0330

    update Header.jsx

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import './Header.css'
+import { FaArrowRightLong } from "react-icons/fa6";
+
 export default function Header() {
   return (
-    <section className='header'>Header</section>
+    <section className='header-container'>
+        <div className="header-top">
+            <img src="/public/svg/label.svg" alt="" />
+            <button className='getYourCard-btn'>get your card<FaArrowRightLong />
+            </button>
+        </div>
+        <div className="header-bottom"></div>
+    </section>
   )
 }
