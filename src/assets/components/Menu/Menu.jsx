a4e46dda0b5000afce47049ad699e965b667cff5
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-12 20:33:14 +0330

    update Menu.jsx

--- src/assets/components/Menu/Menu.jsx
+++ src/assets/components/Menu/Menu.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import './Menu.css'
+import { Link } from "react-router-dom";
+import "./Menu.css";
 export default function Menu() {
   return (
     <ul className="menu-list">
       <li className="menu-item">
-        <Link className="menu-link" to="">Home</Link>
+        <Link className="menu-link" to="">
+          Home
+        </Link>
       </li>
       <li className="menu-item">
-        <Link className="menu-link" to="">Get your card</Link>
+        <Link className="menu-link" to="">
+          Get your card
+        </Link>
       </li>
       <li className="menu-item">
-        <Link className="menu-link" to="">Support center</Link>
+        <Link className="menu-link" to="">
+          Support center
+        </Link>
       </li>
     </ul>
   );
