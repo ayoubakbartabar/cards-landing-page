9f840229c981a8a726f852b3cece88e90f71d506
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-12 20:33:00 +0330

    update App.jsx

--- src/assets/components/App/App.jsx
+++ src/assets/components/App/App.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import './App.css'
-import NavBar from '../NavBar/NavBar'
-import Header from '../Header/Header'
+import React from "react";
+import "./App.css";
+import NavBar from "../NavBar/NavBar";
+import Header from "../Header/Header";
 export default function App() {
   return (
     <>
-      <NavBar/>
-      <Header/>
+      <NavBar />
+      <Header />
     </>
-  )
+  );
 }
-
