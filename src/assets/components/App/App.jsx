02a370d9e07a52a31cb4a1b586ca246322b3102f
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-12 20:26:20 +0330

    update App.jsx & Header.jsx and style

--- src/assets/components/App/App.jsx
+++ src/assets/components/App/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
 import NavBar from '../NavBar/NavBar'
+import Header from '../Header/Header'
 export default function App() {
   return (
     <>
       <NavBar/>
+      <Header/>
     </>
   )
 }
--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <section className='header-container'>
         <div className="header-top">
-            <img src="/public/svg/label.svg" alt="" />
+            <img className='card-svg' src="public/svg/card-2-svgrepo-com.svg" alt="" />
             <button className='getYourCard-btn'>get your card<FaArrowRightLong />
             </button>
         </div>
