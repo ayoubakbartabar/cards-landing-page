957d459902e359b0f0909a687ae463a54b7237fa
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-12 19:19:59 +0330

    update Navbar & Menu styles

--- src/assets/components/Menu/Menu.jsx
+++ src/assets/components/Menu/Menu.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import {Link} from "react-router-dom";
-
+import './Menu.css'
 export default function Menu() {
   return (
     <ul className="menu-list">
       <li className="menu-item">
-        <Link to="">Home</Link>
+        <Link className="menu-link" to="">Home</Link>
       </li>
       <li className="menu-item">
-        <Link to="">Get your card</Link>
+        <Link className="menu-link" to="">Get your card</Link>
       </li>
       <li className="menu-item">
-        <Link to="">Support center</Link>
+        <Link className="menu-link" to="">Support center</Link>
       </li>
     </ul>
   );
