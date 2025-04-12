a26a62eeb20cd4020daacfab57bbfb64631696fc
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-12 20:32:36 +0330

    update Header.jsx

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
 import { FaArrowRightLong } from "react-icons/fa6";
 
 export default function Header() {
   return (
-    <section className='header-container'>
-        <div className="header-top">
-            <img className='card-svg' src="public/svg/card-2-svgrepo-com.svg" alt="" />
-            <button className='getYourCard-btn'>get your card<FaArrowRightLong />
-            </button>
+    <section className="header-container">
+      <div className="header-top">
+        <img
+          className="card-svg"
+          src="public/svg/card-2-svgrepo-com.svg"
+          alt=""
+        />
+        <button className="getYourCard-btn">
+          get your card
+          <FaArrowRightLong />
+        </button>
+      </div>
+      <div className="header-bottom">
+        <div className="header-left-side">
+          <h1 className="header-left-side-title">
+            Create physical and virtual cards for your business
+          </h1>
+          <p className="header-left-side-paragraph">
+            Our landing page template works on all devices, so you only have to
+            set it up once, and get beautiful results forever.
+          </p>
+          <button className="getYourCard-btn">
+            get your card
+            <FaArrowRightLong />
+          </button>
+          <button className="read-doc-btn">Read documentation</button>
         </div>
-        <div className="header-bottom">
-            <div className="header-left-side">
-                <h1 className="header-left-side-title">Create physical and virtual cards for your business</h1>
-                <p className="header-left-side-paragraph">Our landing page template works on all devices, so you only
-                have to set it up once, and get beautiful results forever.</p>
-                <button className='getYourCard-btn'>get your card<FaArrowRightLong />
-            </button>
-            <button className='read-doc-btn'>Read documentation</button>
-            </div>
-        </div>
+      </div>
     </section>
-  )
+  );
 }
