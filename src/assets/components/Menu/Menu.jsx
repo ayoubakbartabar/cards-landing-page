99ec8dff53cea099660aa4cfdde1bd0615ec7cc6
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-12 19:12:15 +0330

    create Menu component and ...

---
+++ src/assets/components/Menu/Menu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Link} from "react-router-dom";
+export default function Menu() {
+  return (
+    <ul className="menu-list">
+      <li className="menu-item">
+        <Link to="">Home</Link>
+      </li>
+    </ul>
+  );
+}
--- src/assets/components/NavBar/NavBar.jsx
+++ src/assets/components/NavBar/NavBar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './NavBar.css'
+import Menu from '../Menu/Menu'
 export default function NavBar() {
   return (
     <section className='navbar-section'>
       <img src="/svg/logo.svg" alt="logo" />
-      
+      <Menu/>
     </section>
   )
 }
