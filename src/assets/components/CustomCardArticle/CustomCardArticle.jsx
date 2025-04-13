066e2ba9251aa3ccbcefce32fcd29a87b1e04d56
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-13 12:38:20 +0330

    Update CustomCardArticle

--- src/assets/components/CustomCardArticle/CustomCardArticle.jsx
+++ src/assets/components/CustomCardArticle/CustomCardArticle.jsx
@@ -1,10 +1,29 @@
 import React from "react";
-import './CustomCardArticle.css'
+import "./CustomCardArticle.css";
 export default function CustomCardArticle() {
   return (
     <section className="custom-card-article-container">
       <div className="custom-card-article-box">
         <img src="public/svg/custom-cards-1.svg" alt="custom-card-svg" />
+        <h4 className="custom-card-article-title">Create custom cards</h4>
+        <p className="custom-card-article-paragraph">
+          Create cards that work exactly as you configured them. Make real-time
+          decisions on charges and spendings.
+        </p>
+      </div>
+
+      <div className="divider-line"></div>
+      <div className="custom-card-article-box">
+        <img src="public/svg/custom-cards-2.svg" alt="custom-card-svg" />
+        <h4 className="custom-card-article-title">Create custom cards</h4>
+        <p className="custom-card-article-paragraph">
+          Create cards that work exactly as you configured them. Make real-time
+          decisions on charges and spendings.
+        </p>
+      </div>
+      <div className="divider-line"></div>
+      <div className="custom-card-article-box">
+        <img src="public/svg/custom-cards-3.svg" alt="custom-card-svg" />
         <h4 className="custom-card-article-title">Create custom cards</h4>
         <p className="custom-card-article-paragraph">
           Create cards that work exactly as you configured them. Make real-time
