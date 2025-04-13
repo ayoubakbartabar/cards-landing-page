cafd0e7b38b0ed4da854473cf68f13ae3052f4aa
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-13 10:52:24 +0330

    Added CustomCardArticle component at App.jsx

--- src/assets/components/App/App.jsx
+++ src/assets/components/App/App.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./App.css";
 import NavBar from "../NavBar/NavBar";
 import Header from "../Header/Header";
+import CustomCardArticle from "../CustomCardArticle/CustomCardArticle";
 export default function App() {
   return (
     <>
       <NavBar />
       <Header />
+      <main>
+        <CustomCardArticle/>
+      </main>
     </>
   );
 }
