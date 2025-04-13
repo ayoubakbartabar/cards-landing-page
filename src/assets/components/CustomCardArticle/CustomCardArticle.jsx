2f9925199f7d970430a37111c5caced96ce91d2b
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-13 10:51:11 +0330

    Create CustomCardArticle component with styles

---
+++ src/assets/components/CustomCardArticle/CustomCardArticle.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './CustomCardArticle.css'
+export default function CustomCardArticle() {
+  return (
+    <section className="custom-card-article-container">
+      <div className="custom-card-article-box">
+        <img src="public/svg/custom-cards-1.svg" alt="custom-card-svg" />
+        <h4 className="custom-card-article-title">Create custom cards</h4>
+        <p className="custom-card-article-paragraph">
+          Create cards that work exactly as you configured them. Make real-time
+          decisions on charges and spendings.
+        </p>
+      </div>
+    </section>
+  );
+}
