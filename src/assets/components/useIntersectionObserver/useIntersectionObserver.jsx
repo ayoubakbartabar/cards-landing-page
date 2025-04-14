49400b194c75c0393d81a6da48bed4c299904b69
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-14 18:03:18 +0330

    create and codeing  useIntersectionObserver.jsx

---
+++ src/assets/components/useIntersectionObserver/useIntersectionObserver.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+
+// Custom hook for IntersectionObserver
+const useIntersectionObserver = (ref, threshold = 0.4) => {
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+          }
+        });
+      },
+      {
+        threshold, // When the element is 40% visible
+      }
+    );
+
+    const currentElement = ref.current;
+    if (currentElement) {
+      observer.observe(currentElement);
+    }
+
+    return () => {
+      if (currentElement) {
+        observer.unobserve(currentElement);
+      }
+    };
+  }, [ref, threshold]);
+};
+
+export default useIntersectionObserver;
