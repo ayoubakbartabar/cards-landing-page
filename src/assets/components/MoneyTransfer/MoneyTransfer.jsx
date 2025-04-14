6893e35cd54e2fff09abe2e1800f20ab3b89df69
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-14 17:14:50 +0330

    Update MoneyTransfer.jsx

---
+++ src/assets/components/MoneyTransfer/MoneyTransfer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './MoneyTransfer.css'
+export default function MoneyTransfer() {
+  return (
+    <section className='money-transfer-container'>
+        
+
+    </section>
+  )
+}
