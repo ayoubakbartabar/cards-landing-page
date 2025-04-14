7510f39b068fc25ffb4cdc5b9de6cd8377861078
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-14 17:18:14 +0330

    Update MoneyTransfer.jsx

--- src/assets/components/MoneyTransfer/MoneyTransfer.jsx
+++ src/assets/components/MoneyTransfer/MoneyTransfer.jsx
@@ -3,7 +3,11 @@ import './MoneyTransfer.css'
 export default function MoneyTransfer() {
   return (
     <section className='money-transfer-container'>
-        
+        <img src='public/image/features-03.png' alt='features-img' className="money-transfer-left-side"/>
+
+        <div className="money-transfer-right-side">
+            
+        </div>
 
     </section>
   )
