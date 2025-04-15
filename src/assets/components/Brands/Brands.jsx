d153da47a20ca93dbd60264b6aad202b386d4a74
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-15 14:50:22 +0330

    Update Brand.jsx

--- src/assets/components/Brands/Brands.jsx
+++ src/assets/components/Brands/Brands.jsx
@@ -1,10 +1,81 @@
-import React from 'react'
+import React , {useRef}from "react";
+import "./Brands.css";
+import useIntersectionObserver from "../useIntersectionObserver/useIntersectionObserver.jsx";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 export default function Brands() {
+  const ref = useRef(null);
+  useIntersectionObserver(ref);
+  // create stores Arrays
+  const physicalStores = [
+    "Starbucks",
+    "Tesco",
+    "American Eagle",
+    "Bershka",
+    "salisbury`s",
+    "Marks & Spencer",
+    "Primark",
+    "SportsDirect",
+  ];
+  const onlineStores = [
+    "Amazon Prime",
+    "Universal Store",
+    "Apple One",
+    "Booking.com",
+    "Netflix",
+    "Deliveroo",
+    "Spotify",
+    "Well Made Clothes",
+  ];
+
   return (
-    <section className='brand-container'>
-        <div className="brand-left-side"></div>
-        <div className="brand-right-side"></div>
+    <section className="brand-container" ref={ref}>
+      <div className="brand-left-side  fade-in-up">
+        <h2 className="brand-left-side-title">
+          Get cashback rewards on your favourite brands
+        </h2>
+        <p className="brand-left-side-paragraph">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <ul className="physical-stores-list">
+          <h4 className="physical-stores-list-title">Physical Stores</h4>
+          {physicalStores.map((item, index) => (
+            <div key={index + 1}>
+              <img
+                className="blue-check"
+                src="public/svg/blue-Check.svg"
+                alt="blue-Check"
+              />
+              <li className="physical-stores-item">{item}</li>
+            </div>
+          ))}
+        </ul>
+        <ul className="online-stores-list">
+          <h4 className="online-stores-list-title">online Stores</h4>
+          {onlineStores.map((item, index) => (
+            <div key={index + 1}>
+              <img
+                className="blue-check"
+                src="public/svg/blue-Check.svg"
+                alt="blue-Check"
+              />
+              <li className="online-stores-item">{item}</li>
+            </div>
+          ))}
+        </ul>
+        <button className="getYourCard-blue-btn">
+          get your card
+          <span className="arrow-icon">
+            <FaArrowRightLong />
+          </span>
+        </button>
+      </div>
+      <img
+        className="brand-right-side  fade-in-up"
+        src="public/image/features-04.png"
+        alt="features-04"
+      />
     </section>
-  )
+  );
 }
