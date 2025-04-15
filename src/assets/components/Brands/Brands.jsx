3ace1a3b42fbac24b9460a46cdd5cc676ae291b6
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-15 14:20:27 +0330

    Update Brand.jsx

---
+++ src/assets/components/Brands/Brands.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Brands() {
+  return (
+    <section className='brand-container'>
+        <div className="brand-left-side"></div>
+        <div className="brand-right-side"></div>
+    </section>
+  )
+}
