e886a8d65ea4c4bcdea6267c5c3c859a52518ac4
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-16 17:04:35 +0330

    Update GetYourCard.jsx

--- src/assets/components/GetYourCard/GetYourCard.jsx
+++ src/assets/components/GetYourCard/GetYourCard.jsx
@@ -96,9 +96,9 @@ export default function GetYourCard() {
       </div>
       <div className="getyourcart-install-guide-container">
             {installGuide.map(item =>(
-                <div className="getyourcart-install-guide-box">
-                    <h4 className="getyourcart-install-guide-title"></h4>
-                    <p className="getyourcart-install-guide-paragraph"></p>
+                <div key={item.id} className="getyourcart-install-guide-box">
+                    <h4 className="getyourcart-install-guide-title">{item.title}</h4>
+                    <p className="getyourcart-install-guide-paragraph">{item.paragraph}</p>
                 </div>
             ))}
       </div>
