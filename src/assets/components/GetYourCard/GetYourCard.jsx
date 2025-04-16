8219b6ecd32fb7b1e6ddb2a8ed69022dd72aa77a
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-16 16:57:07 +0330

    Update GetYourCard.jsx

--- src/assets/components/GetYourCard/GetYourCard.jsx
+++ src/assets/components/GetYourCard/GetYourCard.jsx
@@ -4,8 +4,13 @@ export default function GetYourCard() {
   return (
     <section className="getyourcart-container">
       <div className="getyourcart-top">
-        <h2 className="getyourcart-title"></h2>
-        <p className="getyourcart-paragraph"></p>
+        <h2 className="getyourcart-title">
+          Get started in minutes and connect all your accounts in one place
+        </h2>
+        <p className="getyourcart-paragraph">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
       </div>
       <div className="getyourcart-img-container">
         <img src="" alt="" className="getyourcart-svg" />
