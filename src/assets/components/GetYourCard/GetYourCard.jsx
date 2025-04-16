75c4ced743b84552a4fb3d6aa947c9925ffeed8a
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-16 16:56:42 +0330

    Update GetYourCard.jsx

--- src/assets/components/GetYourCard/GetYourCard.jsx
+++ src/assets/components/GetYourCard/GetYourCard.jsx
@@ -3,7 +3,10 @@ import "./GetYourCard.css";
 export default function GetYourCard() {
   return (
     <section className="getyourcart-container">
-      <div className="getyourcart-top"></div>
+      <div className="getyourcart-top">
+        <h2 className="getyourcart-title"></h2>
+        <p className="getyourcart-paragraph"></p>
+      </div>
       <div className="getyourcart-img-container">
         <img src="" alt="" className="getyourcart-svg" />
         <img src="" alt="" className="getyourcart-img" />
