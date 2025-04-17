cdd19c0213144b427be6d1cc84baf0498fd25f4e
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-17 16:26:13 +0330

    update Header.jsx

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,33 +1,37 @@
 import React from "react";
 import "./Header.css";
 import { TypeAnimation } from "react-type-animation";
+import { useState } from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
 
-export default function Header() {
+const HeaderSection = () => {
+  const [animationDone, setAnimationDone] = useState(false);
+
   return (
     <section className="header-container">
-      <div className="header-top   fade-up delay-1">
+      <div className="header-top fade-up delay-1">
         <svg
           className="card-svg"
           width="30"
           height="30"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <g fill-rule="nonzero" fill="none">
-            <g class="cypkd" fill=' #eff6ff' transform="translate(3 3)">
-              <circle cx="5" cy="5" r="5"></circle>
-              <circle cx="19" cy="5" r="5"></circle>
-              <circle cx="5" cy="19" r="5"></circle>
-              <circle cx="19" cy="19" r="5"></circle>
+          <g fillRule="nonzero" fill="none">
+            <g className="cypkd" fill="#eff6ff" transform="translate(3 3)">
+              <circle cx="5" cy="5" r="5" />
+              <circle cx="19" cy="5" r="5" />
+              <circle cx="5" cy="19" r="5" />
+              <circle cx="19" cy="19" r="5" />
             </g>
-            <g class="coow8" fill=' #7dd3fc'>
-              <circle cx="15" cy="5" r="5"></circle>
-              <circle cx="25" cy="15" r="5"></circle>
-              <circle cx="15" cy="25" r="5"></circle>
-              <circle cx="5" cy="15" r="5"></circle>
+            <g className="coow8" fill="#7dd3fc">
+              <circle cx="15" cy="5" r="5" />
+              <circle cx="25" cy="15" r="5" />
+              <circle cx="15" cy="25" r="5" />
+              <circle cx="5" cy="15" r="5" />
             </g>
           </g>
         </svg>
+
         <button className="getYourCard-btn">
           get your card
           <span className="arrow-icon">
@@ -35,23 +39,43 @@ export default function Header() {
           </span>
         </button>
       </div>
-      <div className="header-bottom   fade-up  delay-2">
+
+      <div className="header-bottom fade-up delay-2">
         <div className="header-left-side">
           <h1 className="header-left-side-title">
-            Create physical and virtual cards for your{" "}
-            <span className="business-span">
-              <img
-                className="circle-bg"
-                src="public/svg/background-circle.svg"
-                alt=""
+            {!animationDone ? (
+              <TypeAnimation
+                sequence={[
+                  "Create physical and virtual cards for your business",
+                  () => setAnimationDone(true),
+                ]}
+                speed={50}
+                wrapper="span"
+                repeat={0}
+                style={{ display: "inline-block" }}
               />
-              <p className="business-text">business</p>
-            </span>
+            ) : (
+              <>
+                Create physical and virtual cards for your{" "}
+                <span className="business-span">
+                  {animationDone && (
+                    <img
+                      className="circle-bg fade-in"
+                      src="public/svg/background-circle.svg"
+                      alt="circle-bg"
+                    />
+                  )}
+                  <p className="business-text">business</p>
+                </span>
+              </>
+            )}
           </h1>
+
           <p className="header-left-side-paragraph">
             Our landing page template works on all devices, so you only have to
             set it up once, and get beautiful results forever.
           </p>
+
           <div className="header-left-side-btns">
             <button className="getYourCard-btn">
               get your card
@@ -62,6 +86,7 @@ export default function Header() {
             <button className="read-doc-btn">Read documentation</button>
           </div>
         </div>
+
         <div className="header-image-wrapper">
           <img
             className="header-transparent-img"
@@ -77,4 +102,6 @@ export default function Header() {
       </div>
     </section>
   );
-}
+};
+
+export default HeaderSection;
