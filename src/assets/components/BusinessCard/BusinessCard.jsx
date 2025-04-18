bf76305ac990d1dceb0055fb1eadc15b6d944a3d
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-18 09:33:40 +0330

    Update BusinessCard.jsx

--- src/assets/components/BusinessCard/BusinessCard.jsx
+++ src/assets/components/BusinessCard/BusinessCard.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import './BusinessCard.css'
+import React from "react";
+import "./BusinessCard.css";
 export default function BusinessCard() {
-
-    // create Array
-    const businessCardsArray = [
-        {id:1 , image:'public/image/testimonial-01.jpg', svg:'public/svg/testimonial-sign-01.svg' , detail:'“ This card is awesome. The app lets me link foreign cards with a new one which makes everything 100 times easier. Like Apple Pay, online shopping without useless phone confirmation. I wish I knew this earlier. ”' , name:'Elisa Koeppel' , jobtitles:'CEO & Co-Founder' },
-        {id:2 , image:'public/image/testimonial-02.jpg', svg:'public/svg/testimonial-sign-02.svg' , detail:'“ This card allows us to achieve compliance with minimal effort, spend practically no time on payments-related customer support, and keep the user experience on our platform. “' , name:'Maria Gress' , jobtitles:'CEO & Co-Founder' }
-    ]
+  // create Array
+  const businessCardsArray = [
+    {
+      id: 1,
+      image: "public/image/testimonial-01.jpg",
+      svg: "public/svg/testimonial-sign-01.svg",
+      detail:
+        "“ This card is awesome. The app lets me link foreign cards with a new one which makes everything 100 times easier. Like Apple Pay, online shopping without useless phone confirmation. I wish I knew this earlier. ”",
+      name: "Elisa Koeppel",
+      jobtitles: "CEO & Co-Founder",
+    },
+    {
+      id: 2,
+      image: "public/image/testimonial-02.jpg",
+      svg: "public/svg/testimonial-sign-02.svg",
+      detail:
+        "“ This card allows us to achieve compliance with minimal effort, spend practically no time on payments-related customer support, and keep the user experience on our platform. “",
+      name: "Maria Gress",
+      jobtitles: "CEO & Co-Founder",
+    },
+  ];
   return (
-    <section className='business-card-container'>
-      <div className="business-card-right-side"></div>  
-      <div className="business-card-left-side"></div>  
+    <section className="business-card-container">
+      <div className="business-card-right-side"></div>
+      <div className="business-card-left-side"></div>
     </section>
-  )
+  );
 }
