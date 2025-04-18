b2816e75141e5e882d06565e955284b4d9b7d6ee
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-18 09:53:02 +0330

    Update BusinessCard.jsx

--- src/assets/components/BusinessCard/BusinessCard.jsx
+++ src/assets/components/BusinessCard/BusinessCard.jsx
@@ -34,12 +34,9 @@ export default function BusinessCard() {
     <section className="business-card-container">
       <div className="business-card-right-side">
         <div className="business-card-box-container">
-            {businessCardsArray.map(item =>(
-
-                <div className="business-card-box">
-                
-            </div>
-            ))}
+          {businessCardsArray.map((item) => (
+            <div className="business-card-box"></div>
+          ))}
         </div>
       </div>
       <div className="business-card-left-side">
