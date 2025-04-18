3379b230fe0d4741bdb9fea283cb773b9a479fe8
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-18 09:21:50 +0330

    Update BusinessCard.jsx

--- src/assets/components/BusinessCard/BusinessCard.jsx
+++ src/assets/components/BusinessCard/BusinessCard.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import './BusinessCard.css'
 export default function BusinessCard() {
   return (
-    <section className='business-card-container'>BusinessCard</section>
+    <section className='business-card-container'>
+      <div className="business-card-right-side"></div>  
+      <div className="business-card-left-side"></div>  
+    </section>
   )
 }
