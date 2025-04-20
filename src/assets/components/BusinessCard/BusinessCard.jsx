49b68deb43a1b1760a0f2f88094635c5ca200ae3
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-20 17:23:20 +0330

    Update BusinessCard.jsx

--- src/assets/components/BusinessCard/BusinessCard.jsx
+++ src/assets/components/BusinessCard/BusinessCard.jsx
@@ -97,25 +97,26 @@ export default function BusinessCard() {
             .slice(currentIndex, currentIndex + 1)
             .map((item) => (
               <div
-              key={item.id}
-              className="business-card-box"
-              style={{ backgroundImage: `url(${item.image})` }}
-            >
-              <div className="business-card-overlay">
-                <img
-                  className="business-card-box-svg"
-                  src={item.svg}
-                  alt="SVG"
-                />
-                <p className="business-card-box-paragraph">{item.detail}</p>
-                <span className="business-card-box-info-container">
-                  <span className="business-card-box-name">{item.name}</span> -
-                  <span className="business-card-box-job-titles">
-                    {item.jobtitles}
+                key={item.id}
+                className="business-card-box"
+                style={{ backgroundImage: `url(${item.image})` }}
+              >
+                <div className="business-card-overlay">
+                  <img
+                    className="business-card-box-svg"
+                    src={item.svg}
+                    alt="SVG"
+                  />
+                  <p className="business-card-box-paragraph">{item.detail}</p>
+                  <span className="business-card-box-info-container">
+                    <span className="business-card-box-name">{item.name}</span>{" "}
+                    -
+                    <span className="business-card-box-job-titles">
+                      {item.jobtitles}
+                    </span>
                   </span>
-                </span>
+                </div>
               </div>
-            </div>
             ))}
 
           {/* Pagination dots */}
