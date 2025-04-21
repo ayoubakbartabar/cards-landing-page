422e75e90b9b7db6686fdc1c3c8d70c4fcf45c59
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-21 17:14:25 +0330

    Update CustomSuperCards.jsx

--- src/assets/components/CustomSuperCards/CustomSuperCards.jsx
+++ src/assets/components/CustomSuperCards/CustomSuperCards.jsx
@@ -3,23 +3,39 @@ import "./CustomSuperCards.css";
 export default function CustomSuperCards() {
   // create data file
   const CustomSuperCardsArray = [
-    {id:1 , cardType:"Starter" , image:"public/image/pricing-01.png" , price:'$0/m' , buttonTitle:"get starter" , title:"Features include" , features: [
-      "Contactless payments",
-      "Mobile payments",
-      "Extra card (optional)",
-      "Free payments worldwide",
-      "Free domestic ATM withdrawals",
-    ], },
-    {id:2 , cardType:"Smart" , image:"public/image/pricing-02.png" , price:'$12/m' , buttonTitle:"get smart" , title:"Everything in Starter, plus" , features: [
-      'Cashback',
-'Travel insurance',
-'3 Extra cards (optional)',
-'Flight insurance',
-'Two-factor authentication',
-'Chatbot and in-app support',
-'Discounted domestic transfers',
-    ], }
-  ]
+    {
+      id: 1,
+      cardType: "Starter",
+      image: "public/image/pricing-01.png",
+      price: "$0/m",
+      buttonTitle: "get starter",
+      title: "Features include",
+      features: [
+        "Contactless payments",
+        "Mobile payments",
+        "Extra card (optional)",
+        "Free payments worldwide",
+        "Free domestic ATM withdrawals",
+      ],
+    },
+    {
+      id: 2,
+      cardType: "Smart",
+      image: "public/image/pricing-02.png",
+      price: "$12/m",
+      buttonTitle: "get smart",
+      title: "Everything in Starter, plus",
+      features: [
+        "Cashback",
+        "Travel insurance",
+        "3 Extra cards (optional)",
+        "Flight insurance",
+        "Two-factor authentication",
+        "Chatbot and in-app support",
+        "Discounted domestic transfers",
+      ],
+    },
+  ];
   return (
     <section className="custom-super-cards-container">
       <div className="custom-super-cards-top">
