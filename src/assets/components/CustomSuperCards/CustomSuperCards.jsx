dba085fe8db490640b46b0195b6d8f322c30246d
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-21 17:04:37 +0330

    Update CustomSuperCards.jsx

--- src/assets/components/CustomSuperCards/CustomSuperCards.jsx
+++ src/assets/components/CustomSuperCards/CustomSuperCards.jsx
@@ -5,7 +5,7 @@ export default function CustomSuperCards() {
     <section className='custom-super-cards-container'>
       <div className="custom-super-cards-top">
         <h1 className="custom-super-cards-title">Get the only custom super card you'll ever need</h1>
-      <p className="custom-super-cards-paragraph"></p>
+      <p className="custom-super-cards-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
       <div className="custom-super-cards-bottom"></div>
     </section>
