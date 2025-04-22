62c1050e83f8f13318088b92639b94eb33e01fc4
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-22 18:41:17 +0330

    Update CustomSuperCards.jsx

--- src/assets/components/CustomSuperCards/CustomSuperCards.jsx
+++ src/assets/components/CustomSuperCards/CustomSuperCards.jsx
@@ -26,6 +26,7 @@ export default function CustomSuperCards() {
                 <ul className="custom-super-cards-list">
                 {item.features.map((feature, index) => (
                   <div key={index + 1} className="custom-super-cards-item-container">
+                    <img src="public/svg/green-Check.svg" alt="green check" className="custom-super-cards-item-svg" />
                     <li className="custom-super-cards-item">{feature}</li>
                   </div>
                 ))}
