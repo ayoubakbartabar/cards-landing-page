64bd2c44442e58dd3c8f5ba1c4ce939ae282ab66
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-23 09:15:33 +0330

    Update CustomSuperCards.css and CustomSuperCards.jsx

--- src/assets/components/CustomSuperCards/CustomSuperCards.jsx
+++ src/assets/components/CustomSuperCards/CustomSuperCards.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./CustomSuperCards.css";
 import CustomSuperCardsArray from "./CustomSuperCardsDatas";
 export default function CustomSuperCards() {
- 
   return (
     <section className="custom-super-cards-container">
       <div className="custom-super-cards-top">
@@ -14,28 +13,37 @@ export default function CustomSuperCards() {
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
       </div>
-      <div className="custom-super-cards-bottom"> 
-          {/* set map for generate elements */}
-          {
-            CustomSuperCardsArray.map(item =>(
-              <div key={item.id} className="custom-super-cards-box">
-                <h4 className="custom-super-cards-box-title">{item.cardType}</h4>
-                <img src={item.image} alt={item.title}  className="custom-super-cards-image"/>
-                <h3 className="custom-super-cards-box-price">{item.price}</h3>
-                <button className={`${item.buttonStyle}`}>{item.buttonTitle}</button>
-                <ul className="custom-super-cards-list">
-                {item.features.map((feature, index) => (
-                  <div key={index + 1} className="custom-super-cards-item-container">
-                    <img src="public/svg/green-Check.svg" alt="green check" className="custom-super-cards-item-svg" />
-                    <li className="custom-super-cards-item">{feature}</li>
-                  </div>
-                ))}
-                </ul>
-              </div>
-              
-              
-            ))
-          }
+      <div className="custom-super-cards-bottom">
+        {/* set map for generate elements */}
+        {CustomSuperCardsArray.map((item) => (
+          <div key={item.id} className="custom-super-cards-box">
+            <h4 className="custom-super-cards-box-title">{item.cardType}</h4>
+            <img
+              src={item.image}
+              alt={item.title}
+              className="custom-super-cards-image"
+            />
+            <h3 className="custom-super-cards-box-price">{item.price}</h3>
+            <button className={`${item.buttonStyle}`}>
+              {item.buttonTitle}
+            </button>
+            <ul className="custom-super-cards-list">
+              {item.features.map((feature, index) => (
+                <div
+                  key={index + 1}
+                  className="custom-super-cards-item-container"
+                >
+                  <img
+                    src="public/svg/green-Check.svg"
+                    alt="green check"
+                    className="custom-super-cards-item-svg"
+                  />
+                  <li className="custom-super-cards-item">{feature}</li>
+                </div>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </section>
   );
