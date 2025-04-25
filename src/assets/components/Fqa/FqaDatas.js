fc0b5afd592f506a94527f57e7b0fff618a2804d
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-25 15:01:50 +0330

    Update FqaDatas.js and create object

--- src/assets/components/Fqa/FqaDatas.js
+++ src/assets/components/Fqa/FqaDatas.js
@@ -1,2 +1,4 @@
 // create array
-const fqaArray=[]
+const fqaArray=[
+    {id:1, question:"How can I add money to my account?", description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad minim veniam.'}
+]
