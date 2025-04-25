597493d1cecdcc0b22444ca3b75854b3c0fb949b
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-25 15:10:51 +0330

    Add Fqa component to App.jsx

--- src/assets/components/Fqa/Fqa.jsx
+++ src/assets/components/Fqa/Fqa.jsx
@@ -4,7 +4,7 @@ import fqaArray from './FqaDatas'
 export default function Fqa() {
   return (
     <section className='fqa-container'>
-        <h1 className="fqa-title"></h1>
+        <h1 className="fqa-title">FAQs</h1>
     </section>
   )
 }
