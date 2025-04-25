12b3b5ff9eb564413e73a0a330db16f57473af96
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-25 15:12:02 +0330

    Update Fqa.jsx

--- src/assets/components/Fqa/Fqa.jsx
+++ src/assets/components/Fqa/Fqa.jsx
@@ -5,6 +5,7 @@ export default function Fqa() {
   return (
     <section className='fqa-container'>
         <h1 className="fqa-title">FAQs</h1>
+        <div className="fqa-box-container"></div>
     </section>
   )
 }
