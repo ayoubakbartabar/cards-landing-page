d149a581955e7ad553d34852f12ee020d098d0ce
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-25 15:02:03 +0330

    Update FqaDatas.js

--- src/assets/components/Fqa/FqaDatas.js
+++ src/assets/components/Fqa/FqaDatas.js
@@ -1,4 +1,9 @@
 // create array
-const fqaArray=[
-    {id:1, question:"How can I add money to my account?", description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad minim veniam.'}
-]
+const fqaArray = [
+  {
+    id: 1,
+    question: "How can I add money to my account?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad minim veniam.",
+  },
+];
