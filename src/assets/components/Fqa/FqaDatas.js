5a5eebfddf0f7bd63cc6aab437deaae053f78691
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-25 15:02:17 +0330

    Update FqaDatas.js

--- src/assets/components/Fqa/FqaDatas.js
+++ src/assets/components/Fqa/FqaDatas.js
@@ -6,4 +6,10 @@ const fqaArray = [
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad minim veniam.",
   },
+  {
+    id: 2,
+    question: "How can I add money to my account?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim ad minim veniam.",
+  },
 ];
