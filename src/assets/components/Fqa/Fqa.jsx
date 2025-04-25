249870a0e8a2e129f6215489fe430658522909dd
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-25 15:12:39 +0330

    Update Fqa.jsx

--- src/assets/components/Fqa/Fqa.jsx
+++ src/assets/components/Fqa/Fqa.jsx
@@ -5,7 +5,11 @@ export default function Fqa() {
   return (
     <section className='fqa-container'>
         <h1 className="fqa-title">FAQs</h1>
-        <div className="fqa-box-container"></div>
+        <div className="fqa-box-container">
+            {fqaArray.map(item =>(
+                <div className="fqa-box"></div>
+            ))}
+        </div>
     </section>
   )
 }
