d7d89388a05f2ab8ae66f4ad95bb6701bb1bc0c5
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-26 14:27:08 +0330

    Update Fqa.jsx

--- src/assets/components/Fqa/Fqa.jsx
+++ src/assets/components/Fqa/Fqa.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import './Fqa.css'
-import fqaArray from './FqaDatas'
+import React, { useRef } from "react";
+import "./Fqa.css";
+import fqaArray from "./FqaDatas";
+import useIntersectionObserver from "../useIntersectionObserver/useIntersectionObserver.jsx";
 export default function Fqa() {
+  const ref = useRef(null);
+  useIntersectionObserver(ref);
   return (
-    <section className='fqa-container'>
-        <h1 className="fqa-title">FAQs</h1>
-        <div className="fqa-box-container">
-            {fqaArray.map(item =>(
-                <div key={item.id} className="fqa-box">
-                    <h2 className="fqa-box-title">{item.question}</h2>
-                    <p className="fqa-box-description">{item.description}</p>
-                </div>
-            ))}
-        </div>
+    <section className="fqa-container" ref={ref}>
+      <h1 className="fqa-title fade-in-up">FAQs</h1>
+      <div className="fqa-box-container fade-in-up">
+        {fqaArray.map((item) => (
+          <div key={item.id} className="fqa-box fade-in-up">
+            <h2 className="fqa-box-title fade-in-up">{item.question}</h2>
+            <p className="fqa-box-description fade-in-up">{item.description}</p>
+          </div>
+        ))}
+      </div>
     </section>
-  )
+  );
 }
