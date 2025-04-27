5eb370c1459561c59e0f8ff83173afef6bc78835
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-27 15:58:15 +0330

    Update Footer.jsx

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -9,12 +9,15 @@ export default function Footer() {
           <h3 className="footer-top-text">What are you waiting for?</h3>
           <h1 className="footer-top-title"></h1>
         </div>
-       <div className="footer-top-right-side"> <button className="getYourCard-blue-btn">
-          get your card
-          <span className="arrow-icon">
-            <FaArrowRightLong />
-          </span>
-        </button></div>
+        <div className="footer-top-right-side">
+          {" "}
+          <button className="getYourCard-blue-btn">
+            get your card
+            <span className="arrow-icon">
+              <FaArrowRightLong />
+            </span>
+          </button>
+        </div>
       </div>
       <div className="footer-bottom-section">
         Get the only custom super card
