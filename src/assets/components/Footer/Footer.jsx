7f4565f830d39f066e8d7792aa17d57e5b44e014
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-27 15:57:39 +0330

    Update Footer.jsx

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -5,8 +5,8 @@ export default function Footer() {
   return (
     <footer className="footer-container">
       <div className="footer-top-section">
-        <h3 className="footer-top-text">What are you waiting for?</h3>
-        <h1 className="footer-top-title"></h1>
+        <div className="footer-top-left-side"><h3 className="footer-top-text">What are you waiting for?</h3>
+        <h1 className="footer-top-title"></h1></div>
          <button className="getYourCard-blue-btn">
                   get your card
                   <span className="arrow-icon">
