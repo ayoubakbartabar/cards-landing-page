c55e5663e9afde44874c64337f5f9c4803e31c46
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-27 15:58:03 +0330

    Update Footer.jsx

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import './Footer.css'
+import "./Footer.css";
 import { FaArrowRightLong } from "react-icons/fa6";
 export default function Footer() {
   return (
     <footer className="footer-container">
       <div className="footer-top-section">
-        <div className="footer-top-left-side"><h3 className="footer-top-text">What are you waiting for?</h3>
-        <h1 className="footer-top-title"></h1></div>
-         <button className="getYourCard-blue-btn">
-                  get your card
-                  <span className="arrow-icon">
-                    <FaArrowRightLong />
-                  </span>
-                </button>
+        <div className="footer-top-left-side">
+          <h3 className="footer-top-text">What are you waiting for?</h3>
+          <h1 className="footer-top-title"></h1>
+        </div>
+       <div className="footer-top-right-side"> <button className="getYourCard-blue-btn">
+          get your card
+          <span className="arrow-icon">
+            <FaArrowRightLong />
+          </span>
+        </button></div>
       </div>
       <div className="footer-bottom-section">
         Get the only custom super card
