878b68715dcd83ca17b1e86a9bdd6ae4a3af648e
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-27 15:54:02 +0330

    Update Footer.jsx

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className='footer-container'>
-        <div className="footer-top-section"></div>
+        <div className="footer-top-section">
+            <h3 className="footer-header-text">What are you waiting for?</h3>
+        </div>
         <div className="footer-bottom-section"></div>
     </footer>
   )
