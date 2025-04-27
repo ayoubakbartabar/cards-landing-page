2dced4c528ae719c33b330f2179335bee3ecd0bf
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-27 15:54:48 +0330

    Update Footer.jsx

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <footer className='footer-container'>
-        <div className="footer-top-section">
-            <h3 className="footer-top-text">What are you waiting for?</h3>
-            <h1 className="footer-top-title"></h1>
-        </div>
-        <div className="footer-bottom-section">Get the only custom super card
-        </div>
+    <footer className="footer-container">
+      <div className="footer-top-section">
+        <h3 className="footer-top-text">What are you waiting for?</h3>
+        <h1 className="footer-top-title"></h1>
+      </div>
+      <div className="footer-bottom-section">
+        Get the only custom super card
+      </div>
     </footer>
-  )
+  );
 }
