92ea04eeca2cc631618e698389902a1d78cf12b0
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-28 12:20:43 +0330

    Update FooterDates.js

--- src/assets/components/Footer/FooterDatas.js
+++ src/assets/components/Footer/FooterDatas.js
@@ -14,32 +14,26 @@ const footerDatas = [
     id: 2,
     title: "Company",
     items: [
-        'About us',
-        'Diversity / Inclusion',
-        'Sustainability',
-        'Code of conduct',
-        'Financial statements',
+      "About us",
+      "Diversity / Inclusion",
+      "Sustainability",
+      "Code of conduct",
+      "Financial statements",
     ],
   },
   {
     id: 3,
     title: "Lifestyle",
     items: [
-        'International products',
-        'Currency exchange',
-        'Lounge & Smart delay',
+      "International products",
+      "Currency exchange",
+      "Lounge & Smart delay",
     ],
   },
   {
     id: 4,
     title: "Company",
-    items: [
-        'Send us an email',
-        'Facebook',
-        'Twitter',
-        'Instagram',
-        'TikTok',
-    ],
+    items: ["Send us an email", "Facebook", "Twitter", "Instagram", "TikTok"],
   },
 ];
 export default footerDatas;
