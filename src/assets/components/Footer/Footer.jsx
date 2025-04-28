ea9421a76ca1c24cd5312200d63c6194e1c03c57
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-28 12:59:52 +0330

    Update Footer.css

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -59,11 +59,11 @@ export default function Footer() {
             </ul>
           ))}
         </div>
-        <img
+        {/* <img
           src="public/svg/footer-illustration.svg"
           alt=""
           className="footer-bottom-image"
-        />
+        /> */}
       </div>
     </footer>
   );
