8005be94c0fa71e514a0cfc77337307e007a6590
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-28 12:37:42 +0330

    Update Footer.jsx

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Footer.css";
 import { FaArrowRightLong } from "react-icons/fa6";
-import footerDatas from "./FooterDatas";
+import footerDatas from "./FooterDatas.js";
 export default function Footer() {
   return (
     <footer className="footer-container">
@@ -24,55 +24,41 @@ export default function Footer() {
         </div>
       </div>
       <div className="footer-bottom-section">
-
-      <div className="footer-bottom-left-side">
-      <svg
-          className="card-svg"
-          width="30"
-          height="30"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g fillRule="nonzero" fill="none">
-            <g className="cypkd" fill="#eff6ff" transform="translate(3 3)">
-              <circle cx="5" cy="5" r="5" />
-              <circle cx="19" cy="5" r="5" />
-              <circle cx="5" cy="19" r="5" />
-              <circle cx="19" cy="19" r="5" />
+        <div className="footer-bottom-left-side">
+          <svg
+            className="card-svg"
+            width="30"
+            height="30"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g fillRule="nonzero" fill="none">
+              <g className="cypkd" fill="#eff6ff" transform="translate(3 3)">
+                <circle cx="5" cy="5" r="5" />
+                <circle cx="19" cy="5" r="5" />
+                <circle cx="5" cy="19" r="5" />
+                <circle cx="19" cy="19" r="5" />
+              </g>
+              <g className="coow8" fill="#7dd3fc">
+                <circle cx="15" cy="5" r="5" />
+                <circle cx="25" cy="15" r="5" />
+                <circle cx="15" cy="25" r="5" />
+                <circle cx="5" cy="15" r="5" />
+              </g>
             </g>
-            <g className="coow8" fill="#7dd3fc">
-              <circle cx="15" cy="5" r="5" />
-              <circle cx="25" cy="15" r="5" />
-              <circle cx="15" cy="25" r="5" />
-              <circle cx="5" cy="15" r="5" />
-            </g>
-          </g>
-        </svg>
-        
-      </div>
-      <div className="footer-bottom-right-side">
-      <svg
-          className="card-svg"
-          width="30"
-          height="30"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g fillRule="nonzero" fill="none">
-            <g className="cypkd" fill="#eff6ff" transform="translate(3 3)">
-              <circle cx="5" cy="5" r="5" />
-              <circle cx="19" cy="5" r="5" />
-              <circle cx="5" cy="19" r="5" />
-              <circle cx="19" cy="19" r="5" />
-            </g>
-            <g className="coow8" fill="#7dd3fc">
-              <circle cx="15" cy="5" r="5" />
-              <circle cx="25" cy="15" r="5" />
-              <circle cx="15" cy="25" r="5" />
-              <circle cx="5" cy="15" r="5" />
-            </g>
-          </g>
-        </svg>
-        
-      </div>
+          </svg>
+        </div>
+        <div className="footer-bottom-right-side">
+          {footerDatas.map((item) => (
+            <ul key={item.id} className="footer-bottom-list">
+              <h3 className="footer-bottom-list-title">{item.title}</h3>
+              {item.items.map((feature, index) => (
+                <li key={index + 1} className="footer-bottom-list-item">
+                  {feature}
+                </li>
+              ))}
+            </ul>
+          ))}
+        </div>
       </div>
     </footer>
   );
