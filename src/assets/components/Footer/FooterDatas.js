44d880a4204395f632e859e7d360b2fb800f0ff8
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-28 12:18:08 +0330

    Update FooterDates.js

--- src/assets/components/Footer/FooterDatas.js
+++ src/assets/components/Footer/FooterDatas.js
@@ -1,2 +1,8 @@
-const footerDatas = []
+const footerDatas = [
+    {id:1,title:'Essentials' , items:['Payments',
+        'Budgeting and analytics',
+        'Open banking',
+        'Pockets',
+        'Subscriptions',]}
+]
 export default footerDatas
