61f865e9ef487276090c78c263676d80818d4c8b
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-28 12:18:26 +0330

    Update FooterDates.js

--- src/assets/components/Footer/FooterDatas.js
+++ src/assets/components/Footer/FooterDatas.js
@@ -10,5 +10,38 @@ const footerDatas = [
       "Subscriptions",
     ],
   },
+  {
+    id: 1,
+    title: "Essentials",
+    items: [
+      "Payments",
+      "Budgeting and analytics",
+      "Open banking",
+      "Pockets",
+      "Subscriptions",
+    ],
+  },
+  {
+    id: 1,
+    title: "Essentials",
+    items: [
+      "Payments",
+      "Budgeting and analytics",
+      "Open banking",
+      "Pockets",
+      "Subscriptions",
+    ],
+  },
+  {
+    id: 1,
+    title: "Essentials",
+    items: [
+      "Payments",
+      "Budgeting and analytics",
+      "Open banking",
+      "Pockets",
+      "Subscriptions",
+    ],
+  },
 ];
 export default footerDatas;
