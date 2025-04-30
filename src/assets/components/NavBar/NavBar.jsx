3b5b53ca20ce7117b8e4ddb5aef56f31a6f1288f
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-04-30 09:18:07 +0330

    Update Navbar Responsive

--- src/assets/components/NavBar/NavBar.jsx
+++ src/assets/components/NavBar/NavBar.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React , {useState} from "react";
 import "./NavBar.css";
 import Menu from "../Menu/Menu";
 export default function NavBar() {
+  // set Hook
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <section className="navbar-section">
       <img src="/svg/logo.svg" alt="logo" />
       <Menu />
-      <button className="mobile-menu"></button>
+      <button className={`mobile-menu ${isOpen ? 'open' : ''}`} onClick={() => setIsOpen(!isOpen)}>
+  <span>link</span>
+  <span>link</span>
+  <span>link</span>
+</button>
     </section>
   );
 }
