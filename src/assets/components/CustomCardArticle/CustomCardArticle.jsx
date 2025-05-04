2592c7cc169db2b8c3966ceae62c75fa81095199
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-04 15:56:16 +0330

    Update CustomCardArticle.jsx and style

--- src/assets/components/CustomCardArticle/CustomCardArticle.jsx
+++ src/assets/components/CustomCardArticle/CustomCardArticle.jsx
@@ -32,6 +32,7 @@ export default function CustomCardArticle() {
   ];
   return (
     <section className="custom-card-article-container" ref={ref}>
+      <div className="custom-card-article-section">
       {customCardArticleArray.map((item) => (
         <div key={item.id} className="custom-card-article-box fade-in-up">
           <img src={item.img} alt="custom-card-svg" />
@@ -39,6 +40,7 @@ export default function CustomCardArticle() {
           <p className="custom-card-article-paragraph">{item.paragraph}</p>
         </div>
       ))}
+      </div>
     </section>
   );
 }
