44a2f2602831c5b839ac917a2a64d3b541c28c74
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-04 15:07:43 +0330

    Update BuildCards.jsx

--- src/assets/components/BuildCards/BuildCards.jsx
+++ src/assets/components/BuildCards/BuildCards.jsx
@@ -37,11 +37,22 @@ export default function BuildCards() {
         </div>
       </div>
       <div className="build-cards-right-side fade-in-up">
-        <img
-          className="build-cards-svg"
-          src="public/svg/cards-illustration.svg"
-          alt="background-svg"
-        />
+       
+        <svg className="build-cards-svg" width="742" height="742" xmlns="http://www.w3.org/2000/svg">
+    <defs>
+        <linearGradient x1="0%" y1="0%" x2="101.743%" y2="100%" id="a">
+            <stop stopColor="#3B82F6" stop-opacity=".64" offset="0%" />
+            <stop stopColor="#3B82F6" stop-opacity="0" offset="100%" />
+        </linearGradient>
+        <filter x="-28.8%" y="-28.8%" width="157.7%" height="157.7%" filterUnits="objectBoundingBox"
+            id="b">
+            <feGaussianBlur stdDeviation="50" in="SourceGraphic" />
+        </filter>
+    </defs>
+    <path
+        d="M177.192 1530.192c0 143.594 116.406 260 260 260s260-116.406 260-260c-.167-143.525-116.475-259.834-260-260-143.594 0-260 116.406-260 260Z"
+        filter="url(#b)" transform="translate(-60 -1153)" fill="url(#a)" fillRule="nonzero" />
+</svg>
         <img
           className="build-cards-img"
           src="public/image/features-02.png"
