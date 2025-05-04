40c911cbfe5e3880bd1c0897593c7d199709e71f
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-04 16:00:14 +0330

    Update MoneyTransfer.jsx and style

--- src/assets/components/MoneyTransfer/MoneyTransfer.jsx
+++ src/assets/components/MoneyTransfer/MoneyTransfer.jsx
@@ -9,38 +9,40 @@ export default function MoneyTransfer() {
   useIntersectionObserver(ref);
   return (
     <section className="money-transfer-container" ref={ref}>
-      <img
-        src="public/image/features-03.png"
-        alt="features-img"
-        className="money-transfer-left-side fade-in-up"
-      />
+      <div className="money-transfer-section">
+        <img
+          src="public/image/features-03.png"
+          alt="features-img"
+          className="money-transfer-left-side fade-in-up"
+        />
 
-      <div className="money-transfer-right-side">
-        <h2 className="money-transfer-title fade-in-up">
-          Spend your money everywhere, pay anyone effortlessly
-        </h2>
-        <p className="money-transfer-paragraph fade-in-up">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua minim
-          veniam, quis nostrud exercitation.
-        </p>
-        <button className="getYourCard-blue-btn fade-in-up">
-          get your card
-          <span className="arrow-icon">
-            <FaArrowRightLong />
-          </span>
-        </button>
-        <div className="money-transfer-left-side-description-container fade-in-up">
-          <img
-            className="money-transfer-left-side-img"
-            src="public/image/quote-author-02.jpg"
-            alt="quote-author"
-          />
-          <p className="money-transfer-left-side-description">
-            “ We know the card market very well and this product provides the
-            speed, flexible account model, and API-first approach that no one
-            else can. ”
+        <div className="money-transfer-right-side">
+          <h2 className="money-transfer-title fade-in-up">
+            Spend your money everywhere, pay anyone effortlessly
+          </h2>
+          <p className="money-transfer-paragraph fade-in-up">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua minim
+            veniam, quis nostrud exercitation.
           </p>
+          <button className="getYourCard-blue-btn fade-in-up">
+            get your card
+            <span className="arrow-icon">
+              <FaArrowRightLong />
+            </span>
+          </button>
+          <div className="money-transfer-left-side-description-container fade-in-up">
+            <img
+              className="money-transfer-left-side-img"
+              src="public/image/quote-author-02.jpg"
+              alt="quote-author"
+            />
+            <p className="money-transfer-left-side-description">
+              “ We know the card market very well and this product provides the
+              speed, flexible account model, and API-first approach that no one
+              else can. ”
+            </p>
+          </div>
         </div>
       </div>
     </section>
