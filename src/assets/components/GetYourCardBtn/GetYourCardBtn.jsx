05a9ddae0cec10c2930fb30439348f3fb68e0976
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-07 20:15:17 +0330

    Update GetYourCardBtn.jsx

---
+++ src/assets/components/GetYourCardBtn/GetYourCardBtn.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./GetYourCardBtn.css";
+import { FaArrowRightLong } from "react-icons/fa6";
+export default function GetYourCardBtn() {
+    
+  // create BuyCardBtnHandler function
+  const BuyCardBtnHandler = () =>{
+    console.log("hello");
+    
+  }
+  return (
+    <>
+      <button onClick={() => BuyCardBtnHandler()} className="getYourCard-btn">
+        get your card
+        <span className="arrow-icon">
+          <FaArrowRightLong />
+        </span>
+      </button>
+    </>
+  );
+}
