dfac91867de08f7ef114d20d8d6f23309d1ca9e5
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-07 20:33:40 +0330

    Update GetYourCardBtn.jsx  and Header.jsx

--- src/assets/components/GetYourCardBtn/GetYourCardBtn.jsx
+++ src/assets/components/GetYourCardBtn/GetYourCardBtn.jsx
@@ -2,16 +2,8 @@ import React from "react";
 import "./GetYourCardBtn.css";
 import { FaArrowRightLong } from "react-icons/fa6";
 export default function GetYourCardBtn() {
-// create const for variant property
-    const className =
-    variant === "blue"
-      ? "getYourCard-blue-btn"
-      : "getYourCard-btn";
 
-  // create BuyCardBtnHandler function
-  const BuyCardBtnHandler = () => {
-    console.log("hello");
-  };
+
   return (
     <>
       <button onClick={() => BuyCardBtnHandler()}  className={className}>
--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -6,91 +6,95 @@ import GetYourCardBtn from "../GetYourCardBtn/GetYourCardBtn";
 
 const HeaderSection = () => {
   const [animationDone, setAnimationDone] = useState(false);
-
+  // create BuyCardBtnHandler function
+  const BuyCardBtnHandler = () => {
+    console.log("hello");
+  };
   return (
     <section className="header-container">
       <div className="header-section">
-      <div className="header-top fade-up delay-1">
-        <svg
-          className="card-svg"
-          width="30"
-          height="30"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g fillRule="nonzero" fill="none">
-            <g className="cypkd" fill="#eff6ff" transform="translate(3 3)">
-              <circle cx="5" cy="5" r="5" />
-              <circle cx="19" cy="5" r="5" />
-              <circle cx="5" cy="19" r="5" />
-              <circle cx="19" cy="19" r="5" />
+        <div className="header-top fade-up delay-1">
+          <svg
+            className="card-svg"
+            width="30"
+            height="30"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g fillRule="nonzero" fill="none">
+              <g className="cypkd" fill="#eff6ff" transform="translate(3 3)">
+                <circle cx="5" cy="5" r="5" />
+                <circle cx="19" cy="5" r="5" />
+                <circle cx="5" cy="19" r="5" />
+                <circle cx="19" cy="19" r="5" />
+              </g>
+              <g className="coow8" fill="#7dd3fc">
+                <circle cx="15" cy="5" r="5" />
+                <circle cx="25" cy="15" r="5" />
+                <circle cx="15" cy="25" r="5" />
+                <circle cx="5" cy="15" r="5" />
+              </g>
             </g>
-            <g className="coow8" fill="#7dd3fc">
-              <circle cx="15" cy="5" r="5" />
-              <circle cx="25" cy="15" r="5" />
-              <circle cx="15" cy="25" r="5" />
-              <circle cx="5" cy="15" r="5" />
-            </g>
-          </g>
-        </svg>
+          </svg>
 
-        <GetYourCardBtn/>
-      </div>
+          <GetYourCardBtn onClick={BuyCardBtnHandler} />
+        </div>
 
-      <div className="header-bottom fade-up delay-2">
-        <div className="header-left-side">
-          <h1 className="header-left-side-title">
-            {!animationDone ? (
-              <TypeAnimation
-                sequence={[
-                  "Create physical and virtual cards for your business",
-                  () => setAnimationDone(true),
-                ]}
-                speed={50}
-                wrapper="span"
-                repeat={0}
-                style={{ display: "inline-block" }}
-              />
-            ) : (
-              <>
-                Create physical and virtual cards for your{" "}
-                <span className="business-span">
-                  {animationDone && (
-                    <img
-                      className="circle-bg fade-in"
-                      src="public/svg/background-circle.svg"
-                      alt="circle-bg"
-                    />
-                  )}
-                  <p className="business-text">business</p>
-                </span>
-              </>
-            )}
-          </h1>
+        <div className="header-bottom fade-up delay-2">
+          <div className="header-left-side">
+            <h1 className="header-left-side-title">
+              {!animationDone ? (
+                <TypeAnimation
+                  sequence={[
+                    "Create physical and virtual cards for your business",
+                    () => setAnimationDone(true),
+                  ]}
+                  speed={50}
+                  wrapper="span"
+                  repeat={0}
+                  style={{ display: "inline-block" }}
+                />
+              ) : (
+                <>
+                  Create physical and virtual cards for your{" "}
+                  <span className="business-span">
+                    {animationDone && (
+                      <img
+                        className="circle-bg fade-in"
+                        src="public/svg/background-circle.svg"
+                        alt="circle-bg"
+                      />
+                    )}
+                    <p className="business-text">business</p>
+                  </span>
+                </>
+              )}
+            </h1>
 
-          <p className="header-left-side-paragraph">
-            Our landing page template works on all devices, so you only have to
-            set it up once, and get beautiful results forever.
-          </p>
+            <p className="header-left-side-paragraph">
+              Our landing page template works on all devices, so you only have
+              to set it up once, and get beautiful results forever.
+            </p>
 
-          <div className="header-left-side-btns">
-          <GetYourCardBtn/>
-            <button className="read-doc-btn">Read documentation</button>
+            <div className="header-left-side-btns">
+              <GetYourCardBtn variant="blue" onClick={BuyCardBtnHandler} />
+
+              <button className="read-doc-btn">Read documentation</button>
+            </div>
+          </div>
+
+          <div className="header-image-wrapper">
+            <img
+              className="header-transparent-img"
+              src="public/svg/logos-illustration.svg"
+              alt=""
+            />
+            <img
+              className="header-img"
+              src="public/image/hero-image.png"
+              alt="hero-image"
+            />
           </div>
         </div>
-
-        <div className="header-image-wrapper">
-          <img
-            className="header-transparent-img"
-            src="public/svg/logos-illustration.svg"
-            alt=""
-          />
-          <img
-            className="header-img"
-            src="public/image/hero-image.png"
-            alt="hero-image"
-          />
-        </div>
-      </div>
       </div>
     </section>
   );
