b12c865354526fb768f2746b613841cbab53bc3f
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-07 19:43:42 +0330

    Add other component in HomPage

--- src/assets/components/App/App.jsx
+++ src/assets/components/App/App.jsx
@@ -1,16 +1,6 @@
 import React from "react";
 import "./App.css";
-import NavBar from "../NavBar/NavBar";
-import Header from "../Header/Header";
-import CustomCardArticle from "../CustomCardArticle/CustomCardArticle";
-import BuildCards from "../BuildCards/BuildCards";
-import MoneyTransfer from "../MoneyTransfer/MoneyTransfer";
-import Brands from "../Brands/Brands";
-import GetYourCard from "../GetYourCard/GetYourCard";
-import BusinessCard from "../BusinessCard/BusinessCard";
-import CustomSuperCards from "../CustomSuperCards/CustomSuperCards";
-import Fqa from "../Fqa/Fqa";
-import Footer from "../Footer/Footer";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 export default function App() {
   return (
--- src/assets/components/HomePage/HomePage.jsx
+++ src/assets/components/HomePage/HomePage.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
-
+import NavBar from "../NavBar/NavBar";
+import Header from "../Header/Header";
+import CustomCardArticle from "../CustomCardArticle/CustomCardArticle";
+import BuildCards from "../BuildCards/BuildCards";
+import MoneyTransfer from "../MoneyTransfer/MoneyTransfer";
+import Brands from "../Brands/Brands";
+import GetYourCard from "../GetYourCard/GetYourCard";
+import BusinessCard from "../BusinessCard/BusinessCard";
+import CustomSuperCards from "../CustomSuperCards/CustomSuperCards";
+import Fqa from "../Fqa/Fqa";
+import Footer from "../Footer/Footer";
 export default function HomePage() {
   return (
     <>
