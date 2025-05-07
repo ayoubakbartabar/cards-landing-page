fad0b9da933cc507e8c8d897b2c18bbab881c09c
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-07 19:46:09 +0330

    Update HomePage.jsx

--- src/assets/components/App/App.jsx
+++ src/assets/components/App/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-
+import HomePage from "../HomePage/HomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 export default function App() {
   return (
     <>
-     
+     <HomePage/>
     </>
   );
 }
