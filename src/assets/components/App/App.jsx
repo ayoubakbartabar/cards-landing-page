ac4673cf2a63d8e6dffd7d01d36cc157bb0b92be
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-07 19:51:46 +0330

    Update App.jsx

--- src/assets/components/App/App.jsx
+++ src/assets/components/App/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import HomePage from "../HomePage/HomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BuyCardPage from "../BuyCardPage/BuyCardPage";
 export default function App() {
   return (
     <Router>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/buy-card" element={<BuyCardComponent />} />
+      <Route path="/buy-card" element={<BuyCardPage/>} />
     </Routes>
   </Router>
   );
