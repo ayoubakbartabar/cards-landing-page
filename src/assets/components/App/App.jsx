54cc6a62fa89941a223300c7ceef15b9e81aef51
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-07 19:51:51 +0330

    Update App.jsx

--- src/assets/components/App/App.jsx
+++ src/assets/components/App/App.jsx
@@ -6,10 +6,10 @@ import BuyCardPage from "../BuyCardPage/BuyCardPage";
 export default function App() {
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/buy-card" element={<BuyCardPage/>} />
-    </Routes>
-  </Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/buy-card" element={<BuyCardPage />} />
+      </Routes>
+    </Router>
   );
 }
