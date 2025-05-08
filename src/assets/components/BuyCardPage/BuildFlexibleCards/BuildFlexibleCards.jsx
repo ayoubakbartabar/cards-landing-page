541b3f38d2f3692f31a37ab92624f120f4a726f3
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-08 18:48:39 +0330

    Update BuildFlexibleCards.jsx

---
+++ src/assets/components/BuyCardPage/BuildFlexibleCards/BuildFlexibleCards.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './BuildFlexibleCards.css'
+export default function BuildFlexibleCards() {
+  return (
+    <section className='build-flexible-card-section'></section>
+  )
+}
