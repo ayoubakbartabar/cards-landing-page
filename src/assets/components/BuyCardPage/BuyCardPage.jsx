a964c2c662415de532570e7c3c83f32e3e78aa7d
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-08 10:21:50 +0330

    Update BuyCardPage.jsx

--- src/assets/components/BuyCardPage/BuyCardPage.jsx
+++ src/assets/components/BuyCardPage/BuyCardPage.jsx
@@ -5,30 +5,29 @@ export default function BuyCardPage() {
     <>
       <section className="buy-card-page-section">
         <div className="buy-card-page-top-side fade-up delay-1">
-          
-            <svg
-              className="card-svg"
-              width="30"
-              height="30"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g fill-rule="nonzero" fill="#3b82f6">
-                <g class="ci3vc" transform="translate(3 3)">
-                  <circle cx="5" cy="5" r="5"></circle>
-                  <circle cx="19" cy="5" r="5"></circle>
-                  <circle cx="5" cy="19" r="5"></circle>
-                  <circle cx="19" cy="19" r="5"></circle>
-                </g>
-                <g class="coow8" fill="#7dd3fc">
-                  <circle cx="15" cy="5" r="5"></circle>
-                  <circle cx="25" cy="15" r="5"></circle>
-                  <circle cx="15" cy="25" r="5"></circle>
-                  <circle cx="5" cy="15" r="5"></circle>
-                </g>
+          <svg
+            className="card-svg"
+            width="30"
+            height="30"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g fill-rule="nonzero" fill="#3b82f6">
+              <g class="ci3vc" transform="translate(3 3)">
+                <circle cx="5" cy="5" r="5"></circle>
+                <circle cx="19" cy="5" r="5"></circle>
+                <circle cx="5" cy="19" r="5"></circle>
+                <circle cx="19" cy="19" r="5"></circle>
               </g>
-            </svg>
-          </div>
-        
+              <g class="coow8" fill="#7dd3fc">
+                <circle cx="15" cy="5" r="5"></circle>
+                <circle cx="25" cy="15" r="5"></circle>
+                <circle cx="15" cy="25" r="5"></circle>
+                <circle cx="5" cy="15" r="5"></circle>
+              </g>
+            </g>
+          </svg>
+        </div>
+
         <div className="buy-card-page-bottom-side"></div>
       </section>
     </>
