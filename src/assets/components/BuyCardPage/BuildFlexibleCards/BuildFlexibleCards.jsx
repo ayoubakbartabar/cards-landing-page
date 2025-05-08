1dc07003224e9f9d84d571035a823288f6c3d686
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-08 18:50:26 +0330

    Update BuildFlexibleCards.jsx

--- src/assets/components/BuyCardPage/BuildFlexibleCards/BuildFlexibleCards.jsx
+++ src/assets/components/BuyCardPage/BuildFlexibleCards/BuildFlexibleCards.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './BuildFlexibleCards.css'
 export default function BuildFlexibleCards() {
   return (
-    <section className='build-flexible-card-section'></section>
+    <section className='build-flexible-card-section'>
+        <h1 className="build-flexible-card-title">Build flexible cards for your business</h1>
+    </section>
   )
 }
