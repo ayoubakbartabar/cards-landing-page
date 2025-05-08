4de524db62d14eff09f10b139821bc9c9736e03b
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-08 10:20:51 +0330

    Update BuyCardPage.jsx

--- src/assets/components/BuyCardPage/BuyCardPage.jsx
+++ src/assets/components/BuyCardPage/BuyCardPage.jsx
@@ -4,7 +4,7 @@ export default function BuyCardPage() {
   return (
     <>
       <section className="buy-card-page-section">
-        <div className="buy-card-page-top-side">
+        <div className="buy-card-page-top-side fade-up delay-1">
           <div className="header-top fade-up delay-1">
             <svg
               className="card-svg"
@@ -12,18 +12,18 @@ export default function BuyCardPage() {
               height="30"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <g fillRule="nonzero" fill="none">
-                <g className="cypkd" fill="#eff6ff" transform="translate(3 3)">
-                  <circle cx="5" cy="5" r="5" />
-                  <circle cx="19" cy="5" r="5" />
-                  <circle cx="5" cy="19" r="5" />
-                  <circle cx="19" cy="19" r="5" />
+              <g fill-rule="nonzero" fill="#3b82f6">
+                <g class="ci3vc" transform="translate(3 3)">
+                  <circle cx="5" cy="5" r="5"></circle>
+                  <circle cx="19" cy="5" r="5"></circle>
+                  <circle cx="5" cy="19" r="5"></circle>
+                  <circle cx="19" cy="19" r="5"></circle>
                 </g>
-                <g className="coow8" fill="#7dd3fc">
-                  <circle cx="15" cy="5" r="5" />
-                  <circle cx="25" cy="15" r="5" />
-                  <circle cx="15" cy="25" r="5" />
-                  <circle cx="5" cy="15" r="5" />
+                <g class="coow8" fill="#7dd3fc">
+                  <circle cx="15" cy="5" r="5"></circle>
+                  <circle cx="25" cy="15" r="5"></circle>
+                  <circle cx="15" cy="25" r="5"></circle>
+                  <circle cx="5" cy="15" r="5"></circle>
                 </g>
               </g>
             </svg>
