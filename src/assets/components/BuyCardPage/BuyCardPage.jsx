1f5e453801f525d50e7bbe71d8c0f89a593969e8
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-08 10:13:26 +0330

    Update BuyCardPage.jsx

--- src/assets/components/BuyCardPage/BuyCardPage.jsx
+++ src/assets/components/BuyCardPage/BuyCardPage.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import './BuyCardPage.css'
+import React from "react";
+import "./BuyCardPage.css";
 export default function BuyCardPage() {
   return (
     <>
-     <section className="buy-card-page-section">
-     <div className="buy-card-page-top-side"></div>
-     <div className="buy-card-page-bottom-side"></div>
-     </section>
+      <section className="buy-card-page-section">
+        <div className="buy-card-page-top-side"></div>
+        <div className="buy-card-page-bottom-side"></div>
+      </section>
     </>
-  )
+  );
 }
