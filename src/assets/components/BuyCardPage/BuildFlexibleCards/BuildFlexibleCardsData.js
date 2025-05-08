4aca40253ec5ed4785f54547f9687bb404670bb5
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-08 18:53:42 +0330

    Update BuildFlexibleCardsData.js

--- src/assets/components/BuyCardPage/BuildFlexibleCards/BuildFlexibleCardsData.js
+++ src/assets/components/BuyCardPage/BuildFlexibleCards/BuildFlexibleCardsData.js
@@ -1,4 +1,8 @@
 const BuildFlexibleCardsData = [
-    {id:1, svg:"public/svg/blue-Check.svg" , paragraph:"Create cards that work exactly as you configured them"}
-]
-export default BuildFlexibleCardsData
+  {
+    id: 1,
+    svg: "public/svg/blue-Check.svg",
+    paragraph: "Create cards that work exactly as you configured them",
+  },
+];
+export default BuildFlexibleCardsData;
