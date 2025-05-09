1f59f74e24762adcdef4f2774b9d097ba235f9e7
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-09 07:56:25 +0330

    Update BuyCardForm.jsx

--- src/assets/components/BuyCardPage/BuyCardForm/BuyCardForm.jsx
+++ src/assets/components/BuyCardPage/BuyCardForm/BuyCardForm.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import './BuyCardForm.css'
+import React from "react";
+import "./BuyCardForm.css";
 export default function BuyCardForm() {
   return (
-    <div className='buy-card-form-container'>
-        <div className="buy-card-form-bg">
-            <img src="public\svg\cards-illustration.svg" alt="svg" />
-            <form className='buy-card-form' action="">
-                
-            </form>
-        </div>
+    <div className="buy-card-form-container">
+      <div className="buy-card-form-bg">
+        <img src="public\svg\cards-illustration.svg" alt="svg" />
+        <form className="buy-card-form" action=""></form>
+      </div>
     </div>
-  )
+  );
 }
