149362b3aa638e8fd2e6fed9b1d4d80c6164cdac
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-09 08:08:20 +0330

    Update BuyCardForm.jsx

--- src/assets/components/BuyCardPage/BuyCardForm/BuyCardForm.jsx
+++ src/assets/components/BuyCardPage/BuyCardForm/BuyCardForm.jsx
@@ -5,13 +5,15 @@ export default function BuyCardForm() {
     <div className="buy-card-form-container">
       <div className="buy-card-form-bg">
         <img src="public\svg\cards-illustration.svg" alt="svg" />
-        <div className="buy-card-form-box">    
-        <form className="buy-card-form">
+        <div className="buy-card-form-box">
+          <form className="buy-card-form">
             <div className="first-name-input-box">
-            <label className="first-name-label" htmlFor="">First name</label>
-            <input className="first-name-input" type="text" />
+              <label className="first-name-label" htmlFor="">
+                First name
+              </label>
+              <input className="first-name-input" type="text" />
             </div>
-        </form>
+          </form>
         </div>
       </div>
     </div>
