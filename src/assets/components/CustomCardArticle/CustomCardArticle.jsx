b283ea29fb179b04213b963d662da7a369e37bad
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-05-14 09:23:16 +0330

    Update CustomCardArticle.jsx img root

--- src/assets/components/CustomCardArticle/CustomCardArticle.jsx
+++ src/assets/components/CustomCardArticle/CustomCardArticle.jsx
@@ -10,21 +10,21 @@ export default function CustomCardArticle() {
   const customCardArticleArray = [
     {
       id: 1,
-      img: "public/svg/custom-cards-1.svg",
+      img: "/svg/custom-cards-1.svg",
       title: "Create custom cards",
       paragraph:
         "Create cards that work exactly as you configured them. Make real-time decisions on charges and spending's.",
     },
     {
       id: 2,
-      img: "public/svg/custom-cards-2.svg",
+      img: "/svg/custom-cards-2.svg",
       title: "Create custom cards",
       paragraph:
         "Create cards that work exactly as you configured them. Make real-time decisions on charges and spending's.",
     },
     {
       id: 3,
-      img: "public/svg/custom-cards-3.svg",
+      img: "/svg/custom-cards-3.svg",
       title: "Create custom cards",
       paragraph:
         "Create cards that work exactly as you configured them. Make real-time decisions on charges and spending's.",
@@ -33,13 +33,13 @@ export default function CustomCardArticle() {
   return (
     <section className="custom-card-article-container" ref={ref}>
       <div className="custom-card-article-section">
-      {customCardArticleArray.map((item) => (
-        <div key={item.id} className="custom-card-article-box fade-in-up">
-          <img src={item.img} alt="custom-card-svg" />
-          <h4 className="custom-card-article-title">{item.title}</h4>
-          <p className="custom-card-article-paragraph">{item.paragraph}</p>
-        </div>
-      ))}
+        {customCardArticleArray.map((item) => (
+          <div key={item.id} className="custom-card-article-box fade-in-up">
+            <img src={item.img} alt="custom-card-svg" />
+            <h4 className="custom-card-article-title">{item.title}</h4>
+            <p className="custom-card-article-paragraph">{item.paragraph}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
